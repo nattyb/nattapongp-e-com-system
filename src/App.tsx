661e7abe46d3c1ed47aaf66e7f003f5e0fcc3482
Nattapong.P <nattapog@ntplc.co.th>
2025-05-21 15:18:33 +0700

    NECS-1 add create sign up ui

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>E-com System 2.0.0 (add Lebel cart button)- updated by Bigg 20250521</h1>
+      <h1>E-com System 2.1.0 (add Lebel cart button)- updated by Bigg 20250521</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
