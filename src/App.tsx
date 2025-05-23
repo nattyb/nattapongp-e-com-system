abbe4413bcad741ec341e73662783832e7d266f1
Nattapong.P <nattapog@ntplc.co.th>
2025-05-23 10:52:09 +0700

    update page error

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>E-com System 7.0.0 20250523</h1>
       <div className="card">
-        <button onClicksdfdsfs={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
